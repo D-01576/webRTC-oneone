ff71b454d1182794082b79708e5a145b8ead5858
Siraj01576 <sarfaraz.8.kh@gmail.com>
2024-12-09 18:15:01 +0500

    first commit

---
+++ src/components/Receiver.jsx
@@ -0,0 +1,73 @@
+import { useEffect, useRef } from "react"
+
+
+export const Receiver = () => {
+    const selfVideo = useRef();
+    const userVideo = useRef();
+    
+    useEffect(() => {
+        const socket = new WebSocket('ws://192.168.18.222:8080');
+        console.log(socket)
+        socket.onopen = () => {
+            socket.send(JSON.stringify({
+                type: 'receiver'
+            }));
+        }
+        startReceiving(socket);
+    }, []);
+
+    function startReceiving(socket) {
+        const pc = new RTCPeerConnection();
+        pc.ontrack = (event) => {
+            console.log(event)
+            userVideo.current.srcObject = new MediaStream([event.track]);
+        }
+
+        pc.onicecandidate = (event) => {
+            if (event.candidate) {
+                socket?.send(JSON.stringify({
+                    type: 'iceCandidate',
+                    candidate: event.candidate
+                }));
+            }
+        }
+
+        socket.onmessage = (event) => {
+            const message = JSON.parse(event.data);
+            if (message.type === 'createOffer') {
+                pc.setRemoteDescription(message.sdp).then(() => {
+                    pc.createAnswer().then((answer) => {
+                        pc.setLocalDescription(answer);
+                        socket.send(JSON.stringify({
+                            type: 'createAnswer',
+                            sdp: answer
+                        }));
+                    });
+                });
+            } else if (message.type === 'iceCandidate') {
+                pc.addIceCandidate(message.candidate);
+            }
+        }
+        getCameraStreamAndSend(pc)
+    }
+
+    const getCameraStreamAndSend = (pc) => {
+        navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+            stream.getTracks().forEach((track) => {
+                pc?.addTrack(track);
+            });
+            selfVideo.current.srcObject = stream;
+        });
+    }
+
+    return <div>
+        <div>
+            <h2>ME</h2>
+        <video autoPlay width={400} height={400} ref={selfVideo} />
+        </div>
+        <div>
+            <h2>YOU</h2>
+        <video autoPlay width={400} height={400} ref={userVideo} />
+        </div>
+    </div>
+}
---
+++ src/components/Sender.jsx
@@ -0,0 +1,83 @@
+import { useEffect, useRef, useState } from "react"
+
+export const Sender = () => {
+    const [socket, setSocket] = useState(null);
+    const [pc, setPC] = useState(null);
+    const selfVideo = useRef();
+    const userVideo = useRef();
+
+    useEffect(() => {
+        const socket = new WebSocket('ws://192.168.18.222:8080');
+        setSocket(socket);
+        socket.onopen = () => {
+            socket.send(JSON.stringify({
+                type: 'sender'
+            }));
+        }
+    }, []);
+
+    const initiateConn = async () => {
+
+        if (!socket) {
+            alert("Socket not found");
+            return;
+        }
+
+        socket.onmessage = async (event) => {
+            const message = JSON.parse(event.data);
+            if (message.type === 'createAnswer') {
+                await pc.setRemoteDescription(message.sdp);
+            } else if (message.type === 'iceCandidate') {
+                pc.addIceCandidate(message.candidate);
+            }
+        }
+
+        const pc = new RTCPeerConnection();
+        setPC(pc);
+        pc.ontrack = (event) => {
+            console.log(event)
+            userVideo.current.srcObject = new MediaStream([event.track]);
+        }
+        pc.onicecandidate = (event) => {
+            if (event.candidate) {
+                socket?.send(JSON.stringify({
+                    type: 'iceCandidate',
+                    candidate: event.candidate
+                }));
+            }
+        }
+
+        pc.onnegotiationneeded = async () => {
+            const offer = await pc.createOffer();
+            await pc.setLocalDescription(offer);
+            socket?.send(JSON.stringify({
+                type: 'createOffer',
+                sdp: pc.localDescription
+            }));
+        }
+            
+        getCameraStreamAndSend(pc);
+    }
+
+    const getCameraStreamAndSend = (pc) => {
+        navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+            stream.getTracks().forEach((track) => {
+                pc?.addTrack(track);
+            });
+            selfVideo.current.srcObject = stream
+        });
+    }
+
+    return <div>
+        Sender
+        <button onClick={initiateConn}> Send data </button>
+        <div>
+            <h2>ME</h2>
+        <video autoPlay width={400} height={400} ref={selfVideo} />
+        </div>
+        <div>
+            <h2>YOU</h2>
+        <video autoPlay width={400} height={400} ref={userVideo} />
+        </div>
+    </div>
+}
